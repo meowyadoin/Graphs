fb43c527aee496eb1b90315d1c20116ca1900419
Lambda_School_Loaner_222 <lambda_school_loaner_222@Lambda-School-Loaner-222s-MacBook-Pro.local>
2020-08-14 09:59:57 -0700

    sprint mvp

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -1,6 +1,7 @@
 from room import Room
 from player import Player
 from world import World
+from util import Queue
 
 import random
 from ast import literal_eval
@@ -14,7 +15,7 @@ world = World()
 # map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-map_file = "maps/main_maze.txt"
+map_file = "/Users/lambda_school_loaner_222/Desktop/Graphs/projects/adventure/maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -29,6 +30,116 @@ player = Player(world.starting_room)
 # traversal_path = ['n', 'n']
 traversal_path = []
 
+map = {}
+
+# convenience method for travel
+def travel_to(direction):
+    player.travel(direction)
+
+# method to create a new room map for all rooms in the graph
+def initialize_room_map():
+    # create a variable to hold the current room
+    current = player.current_room
+    # create an empty dictionary to hold the keys and values
+    # from the room and directions
+    room_map = {}
+
+    # initialize the dictionary to hold values such as { 'n' : '?' }
+    # for all directions leading from the current room
+    for direction in current.get_exits():
+        room_map[direction] = '?'
+        map[current.id] = room_map
+
+# method to pick an unvisited random room
+def unvisited_exit_from_room():
+    # set up the player's current room in a variable
+    current = player.current_room
+    # create an empty array to hold the unvisited rooms
+    unvisited = []
+
+    # loop through all the directions the player can go from the current room,
+    # and only append the rooms that still have a '?'
+    for direction in player.current_room.get_exits():
+        if map[current.id][direction] == '?':
+            unvisited.append(direction)
+
+    # return a randomly chosen room from the array 
+    return random.choice(unvisited)
+
+
+# ---------BFS---------
+def find_unvisited_room(id):
+    # create a queue (BFS requires a queue)
+    # and enqueue the starting vertex in a list (to keep track of the traveled path)
+    # create a visited set to keep track of visited nodes
+    q = Queue()
+    q.enqueue([id])
+    visited = set()
+
+    # while the queue still has items
+    while q.size() > 0:
+        # grab the first item in the queue
+        path = q.dequeue()
+        # and grab the vertex from the last index in the path
+        room = path[-1]
+
+        # if the room isn't in the visited set
+        if room not in visited:
+            # if an array of only '?' values has items,
+            # immediately return the path
+            if list(map[room].values()).count('?') != 0:
+                return path
+            # add it
+            visited.add(room)
+
+            # grab the values from the map pertaining to the current room id
+            # and add them to the path and enqueue the whole path for all rooms
+            for next_room in map[room].values():
+                q.enqueue(path + [next_room])
+
+initialize_room_map()
+
+# set up a while loop to continue traversing while the map dictionary's length
+# is smaller than the number of items in the room graph
+while len(map) < len(room_graph):
+    # if the count of '?' in the map for the current room is not 0
+    if list(map[player.current_room.id].values()).count('?') != 0:
+        # create a variable to hold the current room's id
+        previous = player.current_room.id
+        # grab a random exit that is unvisited
+        next_room = unvisited_exit_from_room()
+        # travel to the random exit
+        # and append it to the traversal path
+        travel_to(next_room)
+        traversal_path.append(next_room)
+        # set up a new pointer to the player's current room after the travel
+        next = player.current_room.id
+
+        # if the next room doesn't have a room map,
+        # initialize one and add it to the map
+        if next not in map:
+            initialize_room_map()
+
+        map[previous][next_room] = next
+
+        # create a reversed dictionary to supply the direction we just came from
+        backtrack_directions = { 'n' : 's' , 's' : 'n','w' : 'e', 'e' : 'w' }
+        # and set the direction to the previous room in this room's map
+        map[next][backtrack_directions[next_room]] = previous
+    else:
+        # now we have no more '?'s to find (end of the road),
+        # we need to backtrack back to the beginning
+        # grab a path to backtrack
+        reverse = find_unvisited_room(player.current_room.id)
+        # for every room in the backwards path,
+        # and every direction in the room, if the direction at the current room's id
+        # equals the room in the reverse path, travel there, and append it to the traversal path
+        for room in reverse:
+            for direction in map[player.current_room.id]:
+                if map[player.current_room.id][direction] == room:
+                    travel_to(direction)
+                    traversal_path.append(direction)
+                    break
 
 
 # TRAVERSAL TEST
@@ -51,12 +162,12 @@ else:
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
